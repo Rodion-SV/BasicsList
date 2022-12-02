1f6931724dd1f3b0673a271afbe95c37a7dadd80
Rodion Samoilov <rodionsamoilov@Rodion-SV.local>
2022-12-02 15:42:24 +0300

    Simple weather app demo

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+let day = "Monday"
+let dailyTemperature = 75
+print ("Today is \(day)! Rise and shine!")
+print ("The temperature on \(day) is \(dailyTemperature) F.")
+var temperature = 70
+print ("The temperature on \(day) morning is \(temperature) F.")
+temperature = 80
+print ("The temperature on \(day) evening is \(temperature) F.")
+let weeklyTemperature = 75
+temperature = weeklyTemperature
+print("The average temperature this week is \(temperature) F.")
+let test = 5
+let test1 = 0.33
+let r = Double(test)+test1
+print(r)

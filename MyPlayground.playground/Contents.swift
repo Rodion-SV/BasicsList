f5a8f01fcb5f7414303efc4419e9c58ac3f59b05
Rodion-SV <rta.sam@icloud.com>
2022-12-03 14:34:37 +0300

    added commenting, output and game's score output

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,15 +1,50 @@
-let day = "Monday"
-let dailyTemperature = 75
-print ("Today is \(day)! Rise and shine!")
-print ("The temperature on \(day) is \(dailyTemperature) F.")
-var temperature = 70
-print ("The temperature on \(day) morning is \(temperature) F.")
-temperature = 80
-print ("The temperature on \(day) evening is \(temperature) F.")
-let weeklyTemperature = 75
-temperature = weeklyTemperature
-print("The average temperature this week is \(temperature) F.")
-let test = 5
-let test1 = 0.33
-let r = Double(test)+test1
-print(r)
+/* Simple temperature output*/
+//let day = "Monday"
+//let dailyTemperature = 75
+//print ("Today is \(day)! Rise and shine!")
+//print ("The temperature on \(day) is \(dailyTemperature) F.")
+//var temperature = 70
+//print ("The temperature on \(day) morning is \(temperature) F.")
+//temperature = 80
+//print ("The temperature on \(day) evening is \(temperature) F.")
+//let weeklyTemperature = 75
+//temperature = weeklyTemperature
+//print("The average temperature this week is \(temperature) F.")
+//let test = 5
+//let test1 = 0.33
+//let r = Double(test)+test1
+//print(r)
+//
+///*Output:
+//Today is Monday! Rise and shine!
+//The temperature on Monday is 75 F.
+//The temperature on Monday morning is 70 F.
+//The temperature on Monday evening is 80 F.
+//The average temperature this week is 75 F.
+//5.33*/
+
+/*simple game score output*/
+let levelScore = 10
+var gameScore = 0
+gameScore += levelScore
+print("The game's score is \(gameScore).")
+var levelBonusScore = 10.0
+levelBonusScore = 20
+print("The level's bonus score is \(levelBonusScore).")
+gameScore += Int(levelBonusScore)
+print("The game's final score is \(gameScore).")
+let levelLowestScore = 50
+let levelHighestScore = 99
+let levels = 10
+var levelScoreDifference = levelHighestScore - levelLowestScore
+var levelAverageScore = levelScoreDifference / levels
+print("The level's average score is \(levelAverageScore).")
+let averageLevelScore = Double(levelScoreDifference) / Double(levels)
+print("The level's average score is \(averageLevelScore).")
+
+/*Output:
+ The game's score is 10.
+ The level's bonus score is 20.0.
+ The game's final score is 30.
+ The level's average score is 4.
+ The level's average score is 4.9.*/

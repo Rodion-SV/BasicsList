8386237e72f2feaff5f6481f89f03ddbfac2ad0e
Rodion-SV <rta.sam@icloud.com>
2022-12-09 22:42:34 +0300

    added passwords for app

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -183,13 +183,65 @@ The average temperature this week is 75 F.
 You rolled a double 2.*/
 
 /*Another method of roll&dice*/
-var firstDice = 0
-var secondDice = 0
-repeat {
-  firstDice = Int.random(in: 1...6)
-  secondDice = Int.random(in: 1...6)
-} while firstDice != secondDice
-print("You rolled a double \(firstDice).")
+//var firstDice = 0
+//var secondDice = 0
+//repeat {
+//  firstDice = Int.random(in: 1...6)
+//  secondDice = Int.random(in: 1...6)
+//} while firstDice != secondDice
+//print("You rolled a double \(firstDice).")
 
 /*Output is random:
  You rolled a double 6.*/
+
+/*App passcodes*/
+var password = "1234"
+let passcode = Int(password)
+print("The passcode of the app is \(passcode!).")
+password = "hello world"
+if let code = Int(password) {
+  print("The passcode of the app is \(code).")
+} else {
+  print("Invalid passcode!")
+}
+let accessCode: Int
+if let code = Int(password) {
+  accessCode = code
+} else {
+  accessCode = 1111
+}
+print("The passcode of the app is \(accessCode).")
+let firstPassword = "hello"
+let secondPassword = "world"
+if let firstPasscode = Int(firstPassword),
+  let secondPasscode =
+    Int(secondPassword)
+{
+  print(
+    "The first passcode of the app is \(firstPasscode) and the second passcode of the app is \(secondPasscode)."
+  )
+} else {
+  print("Invalid passcodes!")
+}
+let firstAccessCode: Int
+let secondAccessCode: Int
+if let firstPasscode = Int(firstPassword),
+  let secondPasscode =
+    Int(secondPassword)
+{
+  firstAccessCode = firstPasscode
+  secondAccessCode = secondPasscode
+} else {
+  firstAccessCode = 1111
+  secondAccessCode = 2222
+}
+print(
+  "The first passcode of the app is \(firstAccessCode) and the second passcode of the app is \(secondAccessCode)."
+)
+
+/*Output:
+ The passcode of the app is 1234.
+ Invalid passcode!
+ The passcode of the app is 1111.
+ Invalid passcodes!
+ The first passcode of the app is 1111 and the second passcode of the app is 2222.*/

0c5a5f2dad1e144aef7f8dd0aa44ab898371ce5c
Rodion-SV <rta.sam@icloud.com>
2022-12-26 18:17:26 +0300

    added user authentification module

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -308,3 +308,19 @@ You rolled a double 2.*/
  The first level's score is 50.
  You have finished playing the free version of the game. Buy the game to play its full version.
  Game restarted!*/
+/*Implementing user authentification*/
+let credentials = ("", -1111)
+if credentials.0 == "" || credentials.1 < 0 {
+  print("Invalid credentials!")
+} else {
+  print("The username is \(credentials.0) and the password is \(credentials.1).")
+}
+let fullCredentials = (password: "pass", passcode: 1111)
+if fullCredentials.password == "" || fullCredentials.passcode < 0 {
+  print("Invalid credentials!")
+} else {
+  print("The password is \(fullCredentials.password) and the passcode is \(fullCredentials.passcode).")
+}
+/*Output:
+ Invalid credentials!
+ The password is pass and the passcode is 1111.*/

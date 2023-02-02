4861f61858f783c7792f4ff7a26c59692aa663a5
Rodion-SV <rta.sam@icloud.com>
2023-02-02 10:00:24 +0300

    full app bank account demo

---
+++ MyPlayground.playground/Pages/Untitled Page 7.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+let levelScore = 10
+var gameScore = 0
+gameScore += levelScore
+print("The game's score is \(gameScore).")
+
+
+var levelBonusScore = 10.0
+levelBonusScore = 20
+print("The level's bonus score is \(levelBonusScore).")

589f3287c081c2f907b61fae96f68b3d11b8ba5c
Rodion-SV <rta.sam@icloud.com>
2023-02-08 13:04:20 +0300

    updated

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -854,28 +854,42 @@ Pasta is not cooked.
 
 /* Menu simulation. */
 
-enum Ingredient: String {
-  case lettuce = "Romaine Lettuce"
-  case croutons = "Salted Croutons"
-  case chicken = "Free Range Chicken"
-}
-let ingredient = Ingredient.lettuce
-switch ingredient {
-case .lettuce:
-  print("We use \(ingredient.rawValue) that has the richest flavor from all the lettuces.")
-case .croutons:
-  print("Our \(ingredient.rawValue) makes sure we do not have to use additional salt in our salads.")
-case .chicken: print("Our meals tastes better with \(ingredient.rawValue) to raise the quality.")
-}
-enum RecipeInformation {
-  case allergens(information: String)
-}
-let recipeInformation = RecipeInformation.allergens(information: "Peanuts, Milk, Gluten")
-switch recipeInformation {
-case .allergens(let information): print("The meal includes the following allergens: \(information).")
-}
+//enum Ingredient: String {
+//  case lettuce = "Romaine Lettuce"
+//  case croutons = "Salted Croutons"
+//  case chicken = "Free Range Chicken"
+//}
+//let ingredient = Ingredient.lettuce
+//switch ingredient {
+//case .lettuce:
+//  print("We use \(ingredient.rawValue) that has the richest flavor from all the lettuces.")
+//case .croutons:
+//  print("Our \(ingredient.rawValue) makes sure we do not have to use additional salt in our salads.")
+//case .chicken: print("Our meals tastes better with \(ingredient.rawValue) to raise the quality.")
+//}
+//enum RecipeInformation {
+//  case allergens(information: String)
+//}
+//let recipeInformation = RecipeInformation.allergens(information: "Peanuts, Milk, Gluten")
+//switch recipeInformation {
+//case .allergens(let information): print("The meal includes the following allergens: \(information).")
+//}
+//
+//var pastaTypesSet = Set<String>()
+//var pastaTypesArray = [String]()
+//pastaTypesSet.insert("Tagliatelle")
+//print(pastaTypesSet)
+//print(pastaTypesSet.count)
+//pastaTypesSet.remove("Spaghetti")
+//let pastaSetUnion = pastaTypesSet.union(pastaTypesSet)
 
 /* Output:
 We use Romaine Lettuce that has the richest flavor from all the lettuces.
 The meal includes the following allergens: Peanuts, Milk, Gluten.
+ ["Tagliatelle"]
+ 1
 */
+
+/* */
+
+
--- MyPlayground.playground/Pages/Untitled Page 8.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Untitled Page 8.xcplaygroundpage/Contents.swift
@@ -1,10 +1,24 @@
-//: [Previous](@previous)
+import XCTest
 
-import Foundation
+class Solution {
+    func countDigits(_ n: Int) -> Int {
+        String(n).compactMap(\.wholeNumberValue).filter{ n.isMultiple(of: $0) }.count
+    }
+}
 
-var r = 4421 % 10
-var rr = 4421
-rr /= 10
+// Executed 2 tests, with 0 failures (0 unexpected) in 0.006 (0.008) seconds
+
+class Tests: XCTestCase {
+    private let s = Solution()
+    
+    func testExample1() {
+        XCTAssert(s.countDigits(7) == 1)
+    }
+    func testExample2() {
+        XCTAssert(s.countDigits(121) == 2)
+    }
+}
+
+Tests.defaultTestSuite.run()
 
 
-//: [Next](@next)

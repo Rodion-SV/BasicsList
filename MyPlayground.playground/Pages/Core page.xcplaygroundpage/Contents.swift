618702112007c552dfba93c8115a0f9a81f36639
Rodion-SV <rta.sam@icloud.com>
2023-02-16 21:43:41 +0300

    delegating extended v1

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -1097,45 +1097,108 @@ Plating artistic level: 10
 */
 
 /* Delegating extended. */
-protocol Driver {
-    var name: String { get }
-    func driveToDestination(_ destination: String, with food: String)
-}
-class DeliveryDriver: Driver {
-    let name: String
-    init(name: String) {
-        self.name = name
-    }
-    func driveToDestination(_ destination: String, with food: String) {
-        print("\(name) is driving to \(destination) to deliver \(food).")
-    }
-}
-class LittleLemon {
-    var deliveryDriver: Driver?
-    func deliverFood(_ food: String, to destination: String) {
-        if let deliveryDriver = deliveryDriver {
-            deliveryDriver.driveToDestination(
-                destination,
-                with: food
-            )
-        } else {
-            print("No delivery driver.")
-        }
-    }
-}
-let bob = DeliveryDriver(name: "Bob")
-let littleLemon = LittleLemon()
-littleLemon.deliverFood(
-    "Super Spaghetti",
-    to: "1 Spaghetti Lane"
-)
-littleLemon.deliveryDriver = bob
-littleLemon.deliverFood(
-    "Super Spaghetti",
-    to: "1 Spaghetti Lane"
-)
+
+//protocol Driver {
+//    var name: String { get }
+//    func driveToDestination(_ destination: String, with food: String)
+//}
+//class DeliveryDriver: Driver {
+//    let name: String
+//    init(name: String) {
+//        self.name = name
+//    }
+//    func driveToDestination(_ destination: String, with food: String) {
+//        print("\(name) is driving to \(destination) to deliver \(food).")
+//    }
+//}
+//class LittleLemon {
+//    var deliveryDriver: Driver?
+//    func deliverFood(_ food: String, to destination: String) {
+//        if let deliveryDriver = deliveryDriver {
+//            deliveryDriver.driveToDestination(
+//                destination,
+//                with: food
+//            )
+//        } else {
+//            print("No delivery driver.")
+//        }
+//    }
+//}
+//let bob = DeliveryDriver(name: "Bob")
+//let littleLemon = LittleLemon()
+//littleLemon.deliverFood(
+//    "Super Spaghetti",
+//    to: "1 Spaghetti Lane"
+//)
+//littleLemon.deliveryDriver = bob
+//littleLemon.deliverFood(
+//    "Super Spaghetti",
+//    to: "1 Spaghetti Lane"
+//)
 
 /* Output:
 No delivery driver.
 Bob is driving to 1 Spaghetti Lane to deliver Super Spaghetti.
 */
+
+/* Delegating extended. */
+
+protocol Protocol1 {
+  func output()
+}
+
+extension Protocol1 {
+  func output() {
+    print("Output")
+  }
+}
+
+class Delegate: Protocol1 {
+  func output() {
+    print("Delegation")
+  }
+}
+
+class Output {
+  var delegate: Protocol1?
+  func output() {
+    delegate?.output()
+  }
+}
+
+let output = Output()
+output.delegate = Delegate()
+output.output()
+
+class Spaghetti {
+    func fetchIngredients() {
+        print("Spaghetti Ingredients")
+    }
+}
+class SpaghettiMeatball: Spaghetti {
+    override func fetchIngredients() {
+        print("BBB")
+        super.fetchIngredients()
+        print("AAA")
+    }
+}
+let spaghettiMeatball = SpaghettiMeatball()
+spaghettiMeatball.fetchIngredients()
+
+protocol Employee {
+    var daysWorking: Int { get set }
+}
+
+struct Waiter: Employee {
+    var daysWorking: Int
+}
+
+print(Waiter.self)
+
+/* Output:
+ Delegation
+ BBB
+ Spaghetti Ingredients
+ AAA
+ Waiter
+ */

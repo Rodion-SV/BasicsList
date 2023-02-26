a7055a0faf23fd5d4a2e63c24966e518dbc6d133
Rodion-SV <rta.sam@icloud.com>
2023-02-26 14:43:07 +0300

    added stacks

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -1409,3 +1409,27 @@ Bob is driving to 1 Spaghetti Lane to deliver Super Spaghetti.
 //        }
 //    }
 //}
+
+
+
+//struct MainView: View {
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                ZStack {
+//                    HStack(spacing: 8) {
+//                        Text("Demo").scaledToFit().frame(
+//                            width: 100, height: 100, alignment: .center)
+//                        VStack(spacing: 10) {
+//                            Text("Little Lemon").font(.title)
+//                            Text("Tomato Tortellini, Bottarga and Carbonara ").font(.title3)
+//                                .multilineTextAlignment(.center)
+//                        }// :- VStack
+//                            .padding()
+//                    }  // :- HStack
+//                    .padding()
+//                }  // :- ZStack
+//            }
+//        }
+//    }
+//}

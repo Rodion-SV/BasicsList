d219a90b8c065d75fac336ee61c5dc4ca75e8ae5
Rodion-SV <rta.sam@icloud.com>
2023-03-03 14:38:26 +0300

    added

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -1495,3 +1495,27 @@ Bob is driving to 1 Spaghetti Lane to deliver Super Spaghetti.
 //        .padding()
 //    }
 //}
+
+
+//struct ContentView: View {
+//    @State var selectedDate = Date()
+//    var dateClosedRange: ClosedRange<Date> {
+//        let min = Calendar.current.date(byAdding: .day, value: -1, to: Date())!
+//        let max = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
+//        return min...max
+//    }
+//    var body: some View {
+//        NavigationView {
+//            Form {
+//                Section {
+//                    DatePicker(
+//                        selection: $selectedDate,
+//                        in: dateClosedRange,
+//                        displayedComponents: .date,
+//                        label: { Text("Due Date") }
+//                    )
+//                }
+//            }
+//        }
+//    }
+//}

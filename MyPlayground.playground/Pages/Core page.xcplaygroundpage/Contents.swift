a5fd9aaa5249bfdc71fdb9ef38db7621c8f04f25
Rodion-SV <rta.sam@icloud.com>
2023-03-04 19:17:04 +0300

    added

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -1519,3 +1519,40 @@ Bob is driving to 1 Spaghetti Lane to deliver Super Spaghetti.
 //        }
 //    }
 //}
+
+
+
+
+//struct SwiftUIView: View {
+//    var body: some View {
+//        Label("Menu", systemImage: "fork.knife")
+//            .labelStyle(.titleAndIcon)
+//    }
+//}
+//
+///*Adding calendar*/
+//struct ReservationCalendar: View {
+//    @State var reservationDate = Date()
+//    var body: some View {
+//        Form {
+//            HStack {
+//                DatePicker(
+//                    selection: $reservationDate, in: Date()...,
+//                    displayedComponents: [.date, .hourAndMinute]
+//                ) {}
+//                Button(action: { print("do something!") }) {
+//                    HStack {
+//                        Image(systemName: "arrow.right.circle")
+//                        Text("Done")
+//                    }
+//                }.padding(20)
+//            }
+//            Text("Date is \(reservationDate.formatted(date: .long, time: .complete))")
+//        }
+//    }
+//}
+//struct ReservationCalendar_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ReservationCalendar()
+//    }
+//}

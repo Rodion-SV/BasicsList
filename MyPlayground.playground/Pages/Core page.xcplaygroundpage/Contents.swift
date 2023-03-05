3be9e4cf52897d12dde708e1bb10cf958f5ca00b
Rodion-SV <rta.sam@icloud.com>
2023-03-05 22:19:01 +0300

    added

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -1556,3 +1556,30 @@ Bob is driving to 1 Spaghetti Lane to deliver Super Spaghetti.
 //        ReservationCalendar()
 //    }
 //}
+
+
+
+//struct ParentView: View {
+//    var body: some View {
+//        HStack {
+//            Rectangle()
+//                .fill(Color.yellow)
+//                .frame(width: 50, height: 50)
+//   
+//            Text("Rectangle One")
+//                .foregroundColor(.white)
+//                .padding(20)
+//                .background(Color.yellow)
+//               
+//            Rectangle()
+//                .fill(Color.blue)
+//                .frame(width: 100, height: 30)
+//        }
+//    }
+//}
+//
+//struct ParentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ParentView()
+//    }
+//}

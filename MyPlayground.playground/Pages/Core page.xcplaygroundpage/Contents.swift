706a3a8041f63f059c01a36a33dc6450b75478d4
Rodion-SV <rta.sam@icloud.com>
2023-03-10 21:10:11 +0300

    added

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -1717,3 +1717,121 @@ Bob is driving to 1 Spaghetti Lane to deliver Super Spaghetti.
 //        ]
 //    }
 //}
+
+
+
+//struct ContentView: View {
+//  var body: some View {
+//    Text("Little Lemon Restaurant").onTapGesture {
+//        print("Text Tapped!")
+//    }
+//  }
+//}
+//
+//struct ContentView: View {
+//  var body: some View {
+//    Text("Little Lemon Restaurant").onTapGesture(count:2) {
+//        print("Text Tapped!")
+//    }
+//  }
+//}
+//
+//struct ContentView: View {
+//  var body: some View {
+//    Text("Little Lemon Restaurant").onLongPressGesture {
+//        print("Long Press Detected!")
+//    }
+//  }
+//}
+//
+//struct ContentView: View {
+//    var body: some View {
+//        Text("Little Lemon Restaurant")
+//            .onLongPressGesture(minimumDuration: 4, maximumDistance: 15, perform: {
+//                print("Long Press Detected!")
+//            },
+//            onPressingChanged: { state in
+//                print (state)
+//            })
+//    }
+//}
+//
+//struct ContentView: View {
+//    @State private var offsetValue = CGSize.zero
+//    var body: some View {
+//        Image("littleLemonLogo")
+//        .offset(offsetValue)
+//            .gesture(
+//                DragGesture()
+//                    .onChanged { gesture in
+//                        offsetValue = gesture.translation
+//                    }
+//            )
+//    }
+//}
+//
+//struct ContentView: View {
+//    @State private var amount = Angle.zero
+//    @State private var finalAmount = Angle.zero
+//    var body: some View {
+//        Image("littleLemonLogo")
+//            .rotationEffect(amount + finalAmount)
+//            .gesture(
+//                RotationGesture()
+//                    .onChanged { value in
+//                        amount = value
+//                    }
+//                    .onEnded { value in
+//                        finalAmount += amount
+//                        amount = .zero
+//                    }
+//            )
+//    }
+//}
+//
+//
+//struct ContentView: View {
+//    @State private var amount = 0.0
+//    @State private var finalAmount = 1.0
+//
+//
+//    var body: some View {
+//        Image("littleLemonRestaurant")
+//            .scaleEffect(finalAmount + amount)
+//            .gesture(
+//        MagnificationGesture()
+//                    .onChanged { value in
+//                        amount = value – 1
+//                    }
+//                    .onEnded { value in
+//                        finalAmount += amount
+//                        amount = 0
+//                    }
+//            )
+//    }
+//}
+//
+//struct ContentView: View {
+//    @State private var message = "You can long press and then drag"
+//
+//
+//    var body: some View {
+//        let longPress = LongPressGesture()
+//            .onEnded { _ in
+//            print("Long Press detected. Now you can drag me")
+//            }
+//
+//
+//        let drag = DragGesture()
+//            .onChanged{ _ in
+//                print ("Dragging…")
+//            }
+//
+//
+//        let sequence = longPress.sequenced(before: drag)
+//
+//
+//        Image("littleLemonLogo")
+//        .gesture(sequence)
+//    }
+//}

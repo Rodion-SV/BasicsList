c53ae8a39dcc58766cfca13b71b556ca9d2da0da
Rodion-SV <rta.sam@icloud.com>
2023-03-22 15:01:55 +0300

    added

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -2190,33 +2190,146 @@ Bob is driving to 1 Spaghetti Lane to deliver Super Spaghetti.
 
 
 
-////Unit testing
-//import XCTest
-//@testable import ImplementUnitTesting
+    ////Unit testing
+    //import XCTest
+    //@testable import ImplementUnitTesting
+    //
+    //final class ImplementUnitTestingTests: XCTestCase {
+    //    func test_calculateTotal_salesTaxTwentyPercent() {
+    //        let items = [CheckoutItem(name: "Chips", price: 49), CheckoutItem(name: "Cola", price: 100), CheckoutItem(name: "Ketchup", price: 400), CheckoutItem(name: "Pizza", price: 982)]
+    //        let taxPercentage = 20
+    //        let totalToPay = calculateTotal(items: items, localTaxPercent: taxPercentage)
+    //        print(totalToPay)
+    //        XCTAssertEqual(totalToPay,1837)
+    //    }
+    //    func testExample() {
+    //     let array = [1, 2, 3, 4]
+    //     let sum = array.reduce(0){$0 + $1}
+    //     XCTAssertNotEqual(sum, 9)
+    //    }
+    //}
+    //
+    //
+    //
+    //
+    //                    .toolbar {
+    //                        ToolbarItem {
+    //                                NavigationLink(destination:MenuItemsOptionView()){Image(systemName: "slider.horizontal.3")}
+    //                                    .frame(maxWidth: .infinity, alignment: .trailing)
+    //
+    //                        }
+    //                    }
+    //                    .navigationTitle("Menu")
+
+
+
+
+//React native example flatlist:
+//import React from 'react';
 //
-//final class ImplementUnitTestingTests: XCTestCase {
-//    func test_calculateTotal_salesTaxTwentyPercent() {
-//        let items = [CheckoutItem(name: "Chips", price: 49), CheckoutItem(name: "Cola", price: 100), CheckoutItem(name: "Ketchup", price: 400), CheckoutItem(name: "Pizza", price: 982)]
-//        let taxPercentage = 20
-//        let totalToPay = calculateTotal(items: items, localTaxPercent: taxPercentage)
-//        print(totalToPay)
-//        XCTAssertEqual(totalToPay,1837)
-//    }
-//    func testExample() {
-//     let array = [1, 2, 3, 4]
-//     let sum = array.reduce(0){$0 + $1}
-//     XCTAssertNotEqual(sum, 9)
-//    }
-//}
+//import { View, Text, StyleSheet, SectionList } from 'react-native';
 //
+//const menuItemsToDisplay = [
+//  {
+//    title: 'Appetizers',
+//    data: [
+//      'Hummus',
+//      'Moutabal',
+//      'Falafel',
+//      'Marinated Olives',
+//      'Kofta',
+//      'Eggplant Salad',
+//    ],
+//  },
+//  {
+//    title: 'Main Dishes',
+//    data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
+//  },
+//  {
+//    title: 'Sides',
+//    data: [
+//      'Fries',
+//      'Buttered Rice',
+//      'Bread Sticks',
+//      'Pita Pocket',
+//      'Lentil Soup',
+//      'Greek Salad',
+//      'Rice Pilaf',
+//    ],
+//  },
+//  {
+//    title: 'Desserts',
+//    data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
+//  },
+//];
 //
+//const Item = ({ name, price }) => (
+//  <View style={menuStyles.innerContainer}>
+//    <Text style={menuStyles.itemText}>{name}</Text>
+//    <Text style={menuStyles.itemText}>{price}</Text>
+//  </View>
+//);
 //
+//const Separator = () => <View style={menuStyles.separator} />;
 //
-//                    .toolbar {
-//                        ToolbarItem {
-//                                NavigationLink(destination:MenuItemsOptionView()){Image(systemName: "slider.horizontal.3")}
-//                                    .frame(maxWidth: .infinity, alignment: .trailing)
+//const Footer = () => (
+//  <Text style={menuStyles.footerText}>
+//    All Rights Reserved by Little Lemon 2022
+//  </Text>
+//);
 //
-//                        }
-//                    }
-//                    .navigationTitle("Menu")
+//const MenuItems = () => {
+//  const renderItem = ({ item }) => <Item name={item} />;
+//
+//  const renderSectionHeader = ({ section: { title } }) => (
+//    <Text style={menuStyles.sectionHeader}>{title} </Text>
+//  );
+//
+//  return (
+//    <View style={menuStyles.container}>
+//      <SectionList
+//        keyExtractor={(item, index) => item + index}
+//        sections={menuItemsToDisplay}
+//        renderItem={renderItem}
+//        renderSectionHeader={renderSectionHeader}
+//        ListFooterComponent={Footer}
+//        ItemSeparatorComponent={Separator}></SectionList>
+//    </View>
+//  );
+//};
+//
+//// Add styles to the component
+//const menuStyles = StyleSheet.create({
+//  container: {
+//    flex: 0.95,
+//  },
+//  innerContainer: {
+//    paddingHorizontal: 40,
+//    paddingVertical: 20,
+//    backgroundColor: '#333333',
+//  },
+//  sectionHeader: {
+//    backgroundColor: '#fbdabb',
+//    color: '#333333',
+//    fontSize: 34,
+//    flexWrap: 'wrap',
+//    textAlign: 'center',
+//  },
+//  itemText: {
+//    color: '#F4CE14',
+//    fontSize: 32,
+//  },
+//  separator: {
+//    borderBottomWidth: 1,
+//    borderColor: '#EDEFEE',
+//  },
+//  footerText: {
+//    color: '#EDEFEE',
+//    fontSize: 20,
+//    flexWrap: 'wrap',
+//    textAlign: 'center',
+//  },
+//});
+//
+//export default MenuItems;
+//

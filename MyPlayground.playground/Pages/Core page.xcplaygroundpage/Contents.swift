88794982fb0dc0e61dffdb7bc335777749367b76
Rodion-SV <rta.sam@icloud.com>
2023-03-27 13:38:50 +0300

    added

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -2765,3 +2765,134 @@ Bob is driving to 1 Spaghetti Lane to deliver Super Spaghetti.
 
 
 
+//Adding pictures in react
+//import React from 'react';
+//import { ScrollView, Image, StyleSheet, Text } from 'react-native';
+//
+//const Welcome = () => {
+//  return (
+//    <ScrollView style={styles.container}>
+//      <Image
+//        style={styles.logo}
+//        source={require('../img/littleLemonLogo.png')}
+//        resizeMode="center"
+//        accessible={true}
+//        accessibilityLabel={'Little Lemon Logo'}
+//      />
+//
+//      <Text style={styles.title}>
+//        Little Lemon, your local Mediterranean Bistro
+//      </Text>
+//      <Image
+//        style={styles.image}
+//        source={require('../img/Picture1.png')}
+//        resizeMode="cover"
+//        accessible={true}
+//        accessibilityLabel={'Little Lemon Logo'}
+//      />
+//      <Image
+//        style={styles.image}
+//        source={require('../img/Picture2.png')}
+//        resizeMode="cover"
+//        accessible={true}
+//        accessibilityLabel={'Little Lemon Logo'}
+//      />
+//      <Image
+//        style={styles.image}
+//        source={require('../img/Picture3.png')}
+//        resizeMode="cover"
+//        accessible={true}
+//        accessibilityLabel={'Little Lemon Logo'}
+//      />
+//      <Image
+//        style={styles.image}
+//        source={require('../img/Picture4.png')}
+//        resizeMode="cover"
+//        accessible={true}
+//        accessibilityLabel={'Little Lemon Logo'}
+//      />
+//    </ScrollView>
+//  );
+//};
+//
+//const styles = StyleSheet.create({
+//  logo: {
+//    height: 100,
+//    width: 300,
+//  },
+//  image: {
+//    width: 350,
+//    height: 250,
+//    borderRadius: 10,
+//  },
+//  container: {
+//    flex: 1,
+//    padding: 24,
+//    marginTop: 25,
+//    backgroundColor: '#fff',
+//  },
+//
+//  title: {
+//    marginTop: 16,
+//    paddingVertical: 10,
+//    color: '#333333',
+//    textAlign: 'center',
+//    fontSize: 20,
+//    fontWeight: 'bold',
+//  },
+//});
+//
+//export default Welcome;
+//
+//
+//
+//Color scheme change react
+//import {
+//  ScrollView,
+//  Image,
+//  StyleSheet,
+//  Text,
+//  useColorScheme,
+//} from 'react-native';
+//
+//const Welcome = () => {
+//  const colorScheme = useColorScheme();
+//
+//  return (
+//    <ScrollView
+//      style={[
+//        styles.container,
+//        colorScheme === 'light'
+//          ? { backgroundColor: '#fff' }
+//          : { backgroundColor: '#333333' },
+//      ]}>
+//           {' '}
+//      <Image
+//        style={styles.logo}
+//        source={require('./img/littleLemonLogo.png')}
+//        resizeMode="center"
+//        accessible={true}
+//        accessibilityLabel={'Little Lemon Logo'}
+//      />
+//          <Text style={styles.regular}>Color Scheme: {colorScheme}</Text>   {' '}
+//    </ScrollView>
+//  );
+//};
+//
+//const styles = StyleSheet.create({
+//  logo: {
+//    height: 100,
+//    width: 300,
+//  },
+//  container: {
+//    flex: 1,
+//    padding: 24,
+//    marginTop: 25,
+//  },
+//  regular: {
+//    fontSize: 18,
+//    textAlign: 'center',
+//  },
+//});
+//
+//export default Welcome;

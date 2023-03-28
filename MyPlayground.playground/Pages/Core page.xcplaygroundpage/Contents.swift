8909e0c0ec796cbf52d7d6d0442bfeb1c5e56f72
Rodion-SV <rta.sam@icloud.com>
2023-03-28 21:18:13 +0300

    added

--- MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Core page.xcplaygroundpage/Contents.swift
@@ -2896,3 +2896,91 @@ Bob is driving to 1 Spaghetti Lane to deliver Super Spaghetti.
 //});
 //
 //export default Welcome;
+
+
+
+
+//React windows dimension
+//
+//
+//import {
+//  ScrollView,
+//  Image,
+//  StyleSheet,
+//  Text,
+//  useWindowDimensions,
+//} from 'react-native';
+//
+//const Welcome = () => {
+//  const window = useWindowDimensions();
+//  return (
+//    <ScrollView style={styles.container}>
+//      <Image
+//        style={styles.logo}
+//        source={require('./img/littleLemonLogo.png')}
+//        resizeMode="center"
+//        accessible={true}
+//        accessibilityLabel={'Little Lemon Logo'}
+//      />
+//      <Text style={styles.title}>
+//        Little Lemon, your local Mediterranean Bistro
+//      </Text>
+//      <Text style={styles.regular}>Window Dimensions</Text>
+//      <Text style={styles.regular}>Height: {window.height}</Text>
+//      <Text style={styles.regular}>Width: {window.width}</Text>
+//      <Text style={styles.regular}>Font scale: {window.fontScale}</Text>
+//    </ScrollView>
+//  );
+//};
+//
+//const styles = StyleSheet.create({
+//  logo: {
+//    height: 100,
+//    width: 300,
+//    resizeMode: 'contain',
+//  },
+//  container: {
+//    flex: 1,
+//    padding: 24,
+//    marginTop: 25,
+//    backgroundColor: '#fff',
+//  },
+//
+//  title: {
+//    marginTop: 16,
+//    paddingVertical: 10,
+//    color: '#333333',
+//    textAlign: 'center',
+//    fontSize: 20,
+//    fontWeight: 'bold',
+//  },
+//});
+//
+//export default Welcome;
+//
+//
+//
+//
+//
+//Changing landing page and its title
+//
+//
+//const Stack = createNativeStackNavigator();
+//
+//export default function App() {
+//  return (
+//    <NavigationContainer>
+//      <Stack.Navigator
+//        initialRouteName="Welcome"
+//        screenOptions={{ headerStyle: { backgroundColor: '#FBDABB' } }}>
+//        <Stack.Screen
+//          name="Welcome"
+//          component={WelcomeScreen}
+//          options={{ title: 'Home' }}
+//        />
+//        <Stack.Screen name="Menu" component={MenuScreen} />
+//      </Stack.Navigator>
+//    </NavigationContainer>
+//  );
+//}
+//

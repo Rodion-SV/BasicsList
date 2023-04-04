98f1447fce25464a689a0edbaa14929d07e69894
Rodion-SV <rta.sam@icloud.com>
2023-04-04 10:02:17 +0300

    added

--- MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -861,3 +861,388 @@
 
 
 
+//492. Construct the Rectangle
+//
+//
+//A web developer needs to know how to design a web page's size. So, given a specific rectangular web page’s area, your job by now is to design a rectangular web page, whose length L and width W satisfy the following requirements:
+//*         The area of the rectangular web page you designed must equal to the given target area.
+//*         The width W should not be larger than the length L, which means L >= W.
+//*         The difference between length L and width W should be as small as possible.
+//Return an array [L, W] where L and W are the length and width of the web page you designed in sequence.
+// 
+//Example 1:
+//Input: area = 4
+//Output: [2,2]
+//Explanation: The target area is 4, and all the possible ways to construct it are [1,4], [2,2], [4,1].
+//But according to requirement 2, [1,4] is illegal; according to requirement 3,  [4,1] is not optimal compared to [2,2]. So the length L is 2, and the width W is 2.
+//Example 2:
+//Input: area = 37
+//Output: [37,1]
+//Example 3:
+//Input: area = 122122
+//Output: [427,286]
+// 
+//Constraints:
+//* 1 <= area <= 107
+//
+//import Foundation
+//
+//class Solution {
+//    func constructRectangle(_ area: Int) -> [Int] {
+//        var tmp = Int(sqrt(Double(area)))
+//        while !area.isMultiple(of: tmp) {
+//            tmp -= 1
+//        }
+//        return [area / tmp, tmp]
+//    }
+//}
+//
+//
+//class Solution {
+//    func constructRectangle(_ area: Int) -> [Int] {
+//        var w = Int(Double(area).squareRoot())
+//        
+//        while w > 1 {
+//            var l = area / w
+//            while w * l < area { l += 1 }
+//            guard w * l != area else { return [l, w] }
+//            w -= 1
+//        }
+//        
+//        return [area, 1]
+//    }
+//}
+//
+//
+//
+//
+//class Solution {
+//    /// Time: O(sqrt(n)). Space: O(1).
+//    func constructRectangle(_ area: Int) -> [Int] {
+//        var rect = [area, 1]
+//
+//        for n in 1...area where n * n <= area {
+//            guard area % n == 0, (area / n - n) < (rect.first! - rect.last!) else { continue }
+//            rect[0] = area / n
+//            rect[1] = n
+//        }
+//        return rect
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//504. Base 7
+//Given an integer num, return a string of its base 7 representation.
+// 
+//Example 1:
+//Input: num = 100
+//Output: "202"
+//Example 2:
+//Input: num = -7
+//Output: "-10"
+// 
+//Constraints:
+//* -107 <= num <= 107
+//
+//
+//
+//class Solution {
+//    /// Time: O(n). Space: O(n).
+//    func convertToBase7(_ num: Int) -> String {
+//        var n = abs(num)
+//        var i = 1
+//        var result = 0
+//
+//        while n > 0 {
+//            result += (n % 7) * i
+//            i *= 10
+//            n /= 7
+//        }
+//        return num.signum() == -1 ? "-\(result)" : "\(result)"
+//    }
+//}
+//
+//
+//class Solution {
+//    func convertToBase7(_ num: Int) -> String {
+//        let baseSevenNum = String(num, radix: 7)
+//        return baseSevenNum
+//    }
+//}
+//
+//
+//class Solution {
+//    func convertToBase7(_ num: Int) -> String {
+//        return String(num, radix: 7)
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//https://leetcode.com/problems/valid-boomerang/description/
+//1037. Valid Boomerang
+//Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+//A boomerang is a set of three points that are all distinct and not in a straight line.
+// 
+//Example 1:
+//Input: points = [[1,1],[2,3],[3,2]]
+//Output: true
+//Example 2:
+//Input: points = [[1,1],[2,2],[3,3]]
+//Output: false
+// 
+//Constraints:
+//* points.length == 3
+//* points[i].length == 2
+//* 0 <= xi, yi <= 100
+//
+//
+//This solution uses the fact that the cross product of two colinear vectors is 0. It subtracts the first point from each of the other points to create two vectrs and then calculates the cross product between the vectors.
+//
+//class Solution {
+//    func isBoomerang(_ p: [[Int]]) -> Bool {
+//        (p[1][0]-p[0][0])*(p[2][1]-p[0][1]) - (p[1][1]-p[0][1])*(p[2][0]-p[0][0]) != 0
+//    }
+//}
+//
+//
+//
+//class Solution {
+//    func isBoomerang(_ points: [[Int]]) -> Bool {
+//        guard Set(points).count >= 3 else { return false }
+//        
+//        let points = points.map { [Double($0[0] + 1), Double($0[1] + 1)] }
+//        func f(_ a: Int,_ b: Int,_ c: Int,_ d: Int) -> Double {abs(points[a][b] - points[c][d])}
+//        
+//        let _0010 = f(0,0,1,0),
+//            _0111 = f(0,1,1,1),
+//            _0020 = f(0,0,2,0),
+//            _0121 = f(0,1,2,1),
+//            _1020 = f(1,0,2,0),
+//            _1121 = f(1,1,2,1)
+//        
+//        let r1 = _0010 / _0111,
+//            r2 = _0020 / _0121,
+//            r3 = _1020 / _1121
+//        
+//        return !(r1 == r2 && r1 == r3)
+//    }
+//}
+//
+//
+//import XCTest
+//
+//// Executed 2 tests, with 0 failures (0 unexpected) in 0.007 (0.008) seconds
+//
+//class Tests: XCTestCase {
+//    private let s = Solution()
+//    func test1() {
+//        let res = s.isBoomerang([[1,1],[2,3],[3,2]])
+//        XCTAssertEqual(res, true)
+//    }
+//    func test2() {
+//        let res = s.isBoomerang([[1,1],[2,2],[3,3]])
+//        XCTAssertEqual(res, false)
+//    }
+//}
+//
+//Tests.defaultTestSuite.run()
+//
+//
+//
+//
+//
+//https://leetcode.com/problems/n-th-tribonacci-number/description/
+//1137. N-th Tribonacci Number
+//The Tribonacci sequence Tn is defined as follows:
+//T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+//Given n, return the value of Tn.
+// 
+//Example 1:
+//Input: n = 4
+//Output: 4
+//Explanation:
+//T_3 = 0 + 1 + 1 = 2
+//T_4 = 1 + 1 + 2 = 4
+//Example 2:
+//Input: n = 25
+//Output: 1389537
+// 
+//Constraints:
+//* 0 <= n <= 37
+//* The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+//
+//
+//
+//class Solution {
+//    
+//    private var cache: [Int: Int] = [0: 0, 1: 1, 2: 1]
+//
+//    func tribonacci(_ n: Int) -> Int {
+//        if let value: Int = cache[n] { return value }
+//        cache[n] = tribonacci(n - 3) + tribonacci(n - 2) + tribonacci(n - 1)
+//        return cache[n]!
+//    }
+//}
+//
+//
+//
+//
+//https://leetcode.com/problems/day-of-the-year/description/
+//1154. Day of the Year
+//
+//Given a string date representing a Gregorian calendar date formatted as YYYY-MM-DD, return the day number of the year.
+// 
+//Example 1:
+//Input: date = "2019-01-09"
+//Output: 9
+//Explanation: Given date is the 9th day of the year in 2019.
+//Example 2:
+//Input: date = "2019-02-10"
+//Output: 41
+// 
+//Constraints:
+//* date.length == 10
+//* date[4] == date[7] == '-', and all other date[i]'s are digits
+//* date represents a calendar date between Jan 1st, 1900 and Dec 31th, 2019.
+//
+//
+//
+//class Solution {
+//    func dayOfYear(_ date: String) -> Int {
+//        var days = [31,28,31,30,31,30,31,31,30,31,30,31]
+//        let date = date.split(separator: "-").map { Int($0)! }
+//        days[1] = date[0].isMultiple(of: 400) || (date[0].isMultiple(of: 4) && !date[0].isMultiple(of: 100)) ? 29 : 28
+//        return days[0..<date[1] - 1].reduce(0, +) + date[2]
+//    }
+//}
+//
+//
+//
+//class Solution {
+//    func dayOfYear(_ dateString: String) -> Int {
+//        let locale = Locale(identifier: "EN_US_POSIX")
+//        let dateFormatter = DateFormatter()
+//        dateFormatter.dateFormat = "YYYY-MM-dd"
+//        dateFormatter.locale = locale
+//        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+//        guard let date = dateFormatter.date(from: dateString) else {
+//            fatalError("Date in incorrect format \(dateString)")
+//        }
+//        return locale.calendar.ordinality(of: .day, in: .year, for: date) ?? -1
+//    }
+//}
+//
+//
+//
+//
+//https://leetcode.com/problems/prime-arrangements/description/
+//1175. Prime Arrangements
+//Return the number of permutations of 1 to n so that prime numbers are at prime indices (1-indexed.)
+//(Recall that an integer is prime if and only if it is greater than 1, and cannot be written as a product of two positive integers both smaller than it.)
+//Since the answer may be large, return the answer modulo 10^9 + 7.
+// 
+//Example 1:
+//Input: n = 5
+//Output: 12
+//Explanation: For example [1,2,5,4,3] is a valid permutation, but [5,2,3,4,1] is not because the prime number 5 is at index 1.
+//Example 2:
+//Input: n = 100
+//Output: 682289015
+// 
+//Constraints:
+//* 1 <= n <= 100
+//
+//
+//
+//
+//class Solution {
+//    func numPrimeArrangements(_ n: Int) -> Int {
+//        
+//        func p(_ x: Int) -> Bool {
+//            guard x > 1 else { return false }
+//            guard x > 3 else { return true }
+//            return (2...(x / 2)).allSatisfy { x % $0 != 0 }
+//        }
+//        
+//        func f(_ x: Int) -> Int {
+//            guard x > 0 else { return 1 }
+//            return (1...x).reduce(into: 1) { $0 = ($0 * $1) % 1000000007 }
+//        }
+//        
+//        let pc = (1...n).filter(p).count
+//        let nc = n - pc
+//        
+//        return f(pc) * f(nc) % 1_000_000_007
+//    }
+//}
+//
+//
+//
+//
+//https://leetcode.com/problems/categorize-box-according-to-criteria/description/
+//2525. Categorize Box According to Criteria
+//Given four integers length, width, height, and mass, representing the dimensions and mass of a box, respectively, return a string representing the category of the box.
+//* The box is "Bulky" if:
+//    * Any of the dimensions of the box is greater or equal to 104.
+//    * Or, the volume of the box is greater or equal to 109.
+//* If the mass of the box is greater or equal to 100, it is "Heavy".
+//* If the box is both "Bulky" and "Heavy", then its category is "Both".
+//* If the box is neither "Bulky" nor "Heavy", then its category is "Neither".
+//* If the box is "Bulky" but not "Heavy", then its category is "Bulky".
+//* If the box is "Heavy" but not "Bulky", then its category is "Heavy".
+//Note that the volume of the box is the product of its length, width and height.
+// 
+//Example 1:
+//Input: length = 1000, width = 35, height = 700, mass = 300
+//Output: "Heavy"
+//Explanation:
+//None of the dimensions of the box is greater or equal to 104.
+//Its volume = 24500000 <= 109. So it cannot be categorized as "Bulky".
+//However mass >= 100, so the box is "Heavy".
+//Since the box is not "Bulky" but "Heavy", we return "Heavy".
+//Example 2:
+//Input: length = 200, width = 50, height = 800, mass = 50
+//Output: "Neither"
+//Explanation:
+//None of the dimensions of the box is greater or equal to 104.
+//Its volume = 8 * 106 <= 109. So it cannot be categorized as "Bulky".
+//Its mass is also less than 100, so it cannot be categorized as "Heavy" either.
+//Since its neither of the two above categories, we return "Neither".
+// 
+//Constraints:
+//* 1 <= length, width, height <= 105
+//* 1 <= mass <= 103
+//
+//
+//
+//
+//
+//class Solution {
+//    func categorizeBox(_ l: Int, _ w: Int, _ h: Int, _ m: Int) -> String {
+//        ["Neither","Bulky","Heavy","Both"][(m>=100 ? 2:0)+((max(l,w,h)>=10000||l*w*h>=1000000000) ? 1:0)]
+//    }
+//}
+//
+//
+//
+//class Solution {
+//    func categorizeBox(_ length: Int, _ width: Int, _ height: Int, _ mass: Int) -> String {
+//        let isBulky = max(length, width, height) >= 10_000 || (length*width*height) >= 1_000_000_000
+//        let isHeavy = mass >= 100
+//        
+//        switch (isBulky, isHeavy) {
+//        case (true, false): return "Bulky"
+//        case (false, true): return "Heavy"
+//        case (true, true): return "Both"
+//        case (false, false): return "Neither"
+//        }
+//    }
+//}

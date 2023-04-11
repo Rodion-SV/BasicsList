c784113d9d74f1c764f66456735f5dd073ff18bf
Rodion-SV <rta.sam@icloud.com>
2023-04-11 20:23:06 +0300

    added

--- MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -3205,3 +3205,72 @@
 //}
 
 
+//https://leetcode.com/problems/remove-outermost-parentheses/
+//1021. Remove Outermost Parentheses
+//A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+//
+//For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+//A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+//
+//Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+//
+//Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "(()())(())"
+//Output: "()()()"
+//Explanation:
+//The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+//After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+//Example 2:
+//
+//Input: s = "(()())(())(()(()))"
+//Output: "()()()()(())"
+//Explanation:
+//The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+//After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+//Example 3:
+//
+//Input: s = "()()"
+//Output: ""
+//Explanation:
+//The input string is "()()", with primitive decomposition "()" + "()".
+//After removing outer parentheses of each part, this is "" + "" = "".
+//
+//
+//Constraints:
+//
+//1 <= s.length <= 105
+//s[i] is either '(' or ')'.
+//s is a valid parentheses string.
+//
+//                                                class Solution {
+//                                                    func removeOuterParentheses(_ S: String) -> String {
+//                                                        var lvl = Int(0)
+//                                                        var result = String()
+//                                                        for char in S {
+//                                                            if char == "(" {
+//                                                                if lvl > 0 {
+//                                                                    result.append(char)
+//                                                                }
+//                                                                lvl += 1
+//                                                            } else {
+//                                                                lvl -= 1
+//                                                                if lvl > 0 {
+//                                                                    result.append(char)
+//                                                                }
+//                                                            }
+//                                                        }
+//                                                        return result
+//                                                    }
+//                                                }
+//
+//class Solution {
+//    func removeOuterParentheses(_ S: String) -> String {
+//        return S.reduce(into: (s: String(), l: Int(0)), { if $1 == "(" { if $0.l > 0 { $0.s.append($1) }; $0.l += 1 } else { $0.l -= 1; if $0.l > 0 { $0.s.append($1) } } }).s
+//    }
+//}
+

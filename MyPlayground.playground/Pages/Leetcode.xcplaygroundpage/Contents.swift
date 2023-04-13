07af0ec226253376f27521aaac1557aedc9e25f1
Rodion-SV <rta.sam@icloud.com>
2023-04-13 21:46:55 +0300

    added

--- MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -3331,3 +3331,40 @@
 //}
 //
 
+
+
+//https://leetcode.com/problems/occurrences-after-bigram/
+//1078. Occurrences After Bigram
+//Given two strings first and second, consider occurrences in some text of the form "first second third", where second comes immediately after first, and third comes immediately after second.
+//
+//Return an array of all the words third for each occurrence of "first second third".
+//
+//
+//
+//Example 1:
+//
+//Input: text = "alice is a good girl she is a good student", first = "a", second = "good"
+//Output: ["girl","student"]
+//Example 2:
+//
+//Input: text = "we will we will rock you", first = "we", second = "will"
+//Output: ["we","rock"]
+//
+//
+//Constraints:
+//
+//1 <= text.length <= 1000
+//text consists of lowercase English letters and spaces.
+//All the words in text a separated by a single space.
+//1 <= first.length, second.length <= 10
+//first and second consist of lowercase English letters.
+//
+//
+//
+//                                            func findOcurrences(_ text: String, _ first: String, _ second: String) -> [String] {
+//                                                let words = text.split(separator: " ")
+//                                                if words.count < 3 { return [] }
+//                                                return (0..<words.count-2).reduce([String]()) {
+//                                                    (words[$1] == first && words[$1+1] == second) ? $0 + [String(words[$1+2])] : $0
+//                                                }
+//                                            }

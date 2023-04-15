260afaec906254fefde9eab50e47a6feeb2bcf49
Rodion-SV <rta.sam@icloud.com>
2023-04-15 15:37:07 +0300

    added

--- MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -3435,3 +3435,46 @@
 //        return output
 //    }
 //}
+
+//https://leetcode.com/problems/largest-substring-between-two-equal-characters/
+//1624. Largest Substring Between Two Equal Characters
+//
+//Given a string s, return the length of the longest substring between two equal characters, excluding the two characters. If there is no such substring return -1.
+//
+//A substring is a contiguous sequence of characters within a string.
+//
+// 
+//
+//Example 1:
+//
+//Input: s = "aa"
+//Output: 0
+//Explanation: The optimal substring here is an empty substring between the two 'a's.
+//Example 2:
+//
+//Input: s = "abca"
+//Output: 2
+//Explanation: The optimal substring here is "bc".
+//Example 3:
+//
+//Input: s = "cbzxy"
+//Output: -1
+//Explanation: There are no characters that appear twice in s.
+//
+//final class Solution {
+//    // O(n) time | O(n) space
+//    func maxLengthBetweenEqualCharacters(_ s: String) -> Int {
+//        var dict: [Character: Int] = [:]
+//        var maximum = -1
+//        
+//        for (index, char) in s.enumerated() {
+//            if let value = dict[char] { // if we find another index, we update our maximum
+//                let newValue = index - value - 1
+//                maximum = max(maximum, newValue)
+//            }
+//            else { dict[char] = index } // we map the first index of a char
+//        }
+//        
+//        return maximum
+//    }
+//}

117d10259d8db52fa6bb5d63002e2faa45e677bb
Rodion-SV <rta.sam@icloud.com>
2023-04-17 14:56:10 +0300

    added

--- MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -3516,3 +3516,66 @@
 //                                                                return count
 //                                                            }
 //                                                        }
+
+//
+//https://leetcode.com/problems/find-the-longest-balanced-substring-of-a-binary-string/
+//2609. Find the Longest Balanced Substring of a Binary String
+//You are given a binary string s consisting only of zeroes and ones.
+//
+//A substring of s is considered balanced if all zeroes are before ones and the number of zeroes is equal to the number of ones inside the substring. Notice that the empty substring is considered a balanced substring.
+//
+//Return the length of the longest balanced substring of s.
+//
+//A substring is a contiguous sequence of characters within a string.
+//
+// 
+//
+//Example 1:
+//
+//Input: s = "01000111"
+//Output: 6
+//Explanation: The longest balanced substring is "000111", which has length 6.
+//Example 2:
+//
+//Input: s = "00111"
+//Output: 4
+//Explanation: The longest balanced substring is "0011", which has length 4.
+//Example 3:
+//
+//Input: s = "111"
+//Output: 0
+//Explanation: There is no balanced substring except the empty substring, so the answer is 0.
+//                                            
+//                                            class Solution {
+//                                                private var n = 0
+//                                                func findTheLongestBalancedSubstring(_ s: String) -> Int {
+//                                                    var s = Array(s).map { Int(String($0))! }
+//                                                    self.n = s.count
+//                                                    var maxValue = 0
+//                                                    var middle = n / 2
+//                                                    var next = middle + 1
+//                                                    for i in 0..<n {
+//                                                        let value = getLength(i, i + 1, s)
+//                                                        maxValue = max(maxValue, value)
+//                                                    }
+//                                                    return maxValue * 2
+//                                                }
+//                                                
+//                                                private func getLength(_ middle: Int, _ next: Int, _ s: [Int]) -> Int {
+//                                                    var counter = 0
+//                                                    var middle = middle
+//                                                    var next = next
+//                                                    while (middle >= 0 && next < n) {
+//                                                        let val1 = s[middle]
+//                                                        let val2 = s[next]
+//                                                        if val1 == 0 && val2 == 1 {
+//                                                            counter += 1
+//                                                        } else {
+//                                                            return counter
+//                                                        }
+//                                                        middle -= 1
+//                                                        next += 1
+//                                                    }
+//                                                    return counter
+//                                                }
+//                                            }

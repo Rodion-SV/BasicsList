63704856cd62b254b15f4ecf91aa53d960f78aa2
Rodion-SV <rta.sam@icloud.com>
2023-04-18 14:02:49 +0300

    added

--- MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -3579,3 +3579,67 @@
 //                                                    return counter
 //                                                }
 //                                            }
+
+
+
+//https://leetcode.com/problems/goal-parser-interpretation/
+//1678. Goal Parser Interpretation
+//You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then concatenated in the original order.
+//
+//Given the string command, return the Goal Parser's interpretation of command.
+//
+//
+//
+//Example 1:
+//
+//Input: command = "G()(al)"
+//Output: "Goal"
+//Explanation: The Goal Parser interprets the command as follows:
+//G -> G
+//() -> o
+//(al) -> al
+//The final concatenated result is "Goal".
+//Example 2:
+//
+//Input: command = "G()()()()(al)"
+//Output: "Gooooal"
+//Example 3:
+//
+//Input: command = "(al)G(al)()()G"
+//Output: "alGalooG"
+//
+//
+//Constraints:
+//
+//1 <= command.length <= 100
+//command consists of "G", "()", and/or "(al)" in some order.
+//
+//
+//func interpret(_ command: String) -> String {
+//        let st = Array(command)
+//        var fin = "", i = 0
+//        while(i < st.count){
+//            if st[i] == "G"{
+//                fin += "G"
+//                i += 1
+//            }
+//            else{
+//                if st[i + 1] == ")"{
+//                    fin += "o"
+//                    i += 2
+//                }
+//                else{
+//                    fin += "al"
+//                    i += 4
+//                }
+//            }
+//        }
+//        return fin
+//    }
+//
+//class Solution {
+//    func interpret(_ command: String) -> String {
+//         return command.replacingOccurrences(of:"()", with:"o").replacingOccurrences(of:"(al)", with:"al")
+//     }
+//}
+

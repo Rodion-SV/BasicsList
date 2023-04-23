cc9a7833fa492d3059256188511054e548072093
Rodion-SV <rta.sam@icloud.com>
2023-04-23 12:48:22 +0300

    added

--- MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -3831,3 +3831,83 @@
 //        return true
 //    }
 //}
+
+
+//https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/
+//2114. Maximum Number of Words Found in Sentences
+//A sentence is a list of words that are separated by a single space with no leading or trailing spaces.
+//
+//You are given an array of strings sentences, where each sentences[i] represents a single sentence.
+//
+//Return the maximum number of words that appear in a single sentence.
+//
+// 
+//
+//Example 1:
+//
+//Input: sentences = ["alice and bob love leetcode", "i think so too", "this is great thanks very much"]
+//Output: 6
+//Explanation:
+//- The first sentence, "alice and bob love leetcode", has 5 words in total.
+//- The second sentence, "i think so too", has 4 words in total.
+//- The third sentence, "this is great thanks very much", has 6 words in total.
+//Thus, the maximum number of words in a single sentence comes from the third sentence, which has 6 words.
+//Example 2:
+//
+//Input: sentences = ["please wait", "continue to fight", "continue to win"]
+//Output: 3
+//Explanation: It is possible that multiple sentences contain the same number of words.
+//In this example, the second and third sentences (underlined) have the same number of words.
+// 
+//
+//Constraints:
+//
+//1 <= sentences.length <= 100
+//1 <= sentences[i].length <= 100
+//sentences[i] consists only of lowercase English letters and ' ' only.
+//sentences[i] does not have leading or trailing spaces.
+//All the words in sentences[i] are separated by a single space.
+//
+//
+//
+//class Solution {
+//    func mostWordsFound(_ sentences: [String]) -> Int {
+//        sentences.map { $0.components(separatedBy: " ") }.map { $0.count }.max() ?? 0
+//    }
+//}
+//
+//class Solution {
+//    func mostWordsFound(_ sentences: [String]) -> Int {
+//        
+//        var countSpace = 0
+//        var count = 0
+//
+//        for i in sentences[0] {
+//            i == " " ? (countSpace += 1) : (countSpace += 0)
+//        }
+//
+//        for i in sentences {
+//            for j in i {
+//                j == " " ? (count += 1) : (count += 0)
+//            }
+//
+//            if count > countSpace {
+//                countSpace = count
+//                count = 0
+//            } else {
+//                count = 0
+//            }
+//
+//        }
+//
+//        return countSpace + 1
+//    }
+//}
+//
+//class Solution {
+//    func mostWordsFound(_ sentences: [String]) -> Int {
+//        var ceil = 0
+//        sentences.forEach { ceil = max(ceil, $0.components(separatedBy: " ").count)}
+//        return ceil
+//    }
+//}

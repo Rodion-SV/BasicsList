7575d8b1eaf5a4f4b911c94da0cf6c2b1d69b3eb
Rodion-SV <rta.sam@icloud.com>
2023-04-30 15:54:01 +0300

    added

--- MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Leetcode.xcplaygroundpage/Contents.swift
@@ -4316,3 +4316,82 @@
 //    return (k <= distinct.count) ? distinct[k-1] : ""
 //}
 //}
+
+//https://leetcode.com/problems/number-of-valid-words-in-a-sentence/
+//2047. Number of Valid Words in a Sentence
+//A sentence consists of lowercase letters ('a' to 'z'), digits ('0' to '9'), hyphens ('-'), punctuation marks ('!', '.', and ','), and spaces (' ') only. Each sentence can be broken down into one or more tokens separated by one or more spaces ' '.
+//
+//A token is a valid word if all three of the following are true:
+//
+//It only contains lowercase letters, hyphens, and/or punctuation (no digits).
+//There is at most one hyphen '-'. If present, it must be surrounded by lowercase characters ("a-b" is valid, but "-ab" and "ab-" are not valid).
+//There is at most one punctuation mark. If present, it must be at the end of the token ("ab,", "cd!", and "." are valid, but "a!b" and "c.," are not valid).
+//Examples of valid words include "a-b.", "afad", "ba-c", "a!", and "!".
+//
+//Given a string sentence, return the number of valid words in sentence.
+//
+//
+//
+//Example 1:
+//
+//Input: sentence = "cat and  dog"
+//Output: 3
+//Explanation: The valid words in the sentence are "cat", "and", and "dog".
+//Example 2:
+//
+//Input: sentence = "!this  1-s b8d!"
+//Output: 0
+//Explanation: There are no valid words in the sentence.
+//"!this" is invalid because it starts with a punctuation mark.
+//"1-s" and "b8d" are invalid because they contain digits.
+//Example 3:
+//
+//Input: sentence = "alice and  bob are playing stone-game10"
+//Output: 5
+//Explanation: The valid words in the sentence are "alice", "and", "bob", "are", and "playing".
+//"stone-game10" is invalid because it contains digits.
+//
+//
+//Constraints:
+//
+//1 <= sentence.length <= 1000
+//sentence only contains lowercase English letters, digits, ' ', '-', '!', '.', and ','.
+//There will be at least 1 token.
+//
+//
+//                            class Solution {
+//                                func countValidWords(_ sentence: String) -> Int {
+//                                    var count = 0
+//                                    let punctuation = ".,!"
+//
+//                                    for word in sentence.split(separator: " ") where word == word.lowercased() {
+//                                        var valid = true
+//                                        var hyphenSeen = false
+//
+//                                        for (index, char) in word.enumerated() {
+//                                            if char.isNumber || (punctuation.contains(char) && index != word.count - 1) {
+//                                                valid = false
+//                                                break
+//                                            }
+//
+//                                            if char == "-" {
+//                                                let lastChar = word[word.index(word.endIndex, offsetBy: -1)]
+//
+//                                                if hyphenSeen || index == 0 || index == word.count - 1
+//                                                    || index == word.count - 2 && punctuation.contains(lastChar) {
+//                                                    valid = false
+//                                                    break
+//                                                }
+//
+//                                                hyphenSeen = true
+//                                            }
+//                                        }
+//
+//                                        if valid {
+//                                            count += 1
+//                                        }
+//                                    }
+//
+//                                    return count
+//                                }
+//                            }
